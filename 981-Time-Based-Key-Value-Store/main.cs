c260442a6432963ac9ce86348ef9eacfd2f69ce3
dzeleniak <david.zeleniak@kcc.com>
2022-10-07 09:45:24 -0700

    981

---
+++ 981-Time-Based-Key-Value-Store/main.cs
@@ -0,0 +1,36 @@
+public class TimeMap {
+
+    Dictionary<string, Dictionary<int, string>> timeMap;
+    
+    public TimeMap() {
+        timeMap = new Dictionary<string, Dictionary<int, string>>();
+    }
+    
+    public void Set(string key, string value, int timestamp) {
+        if(!timeMap.ContainsKey(key)) {
+            timeMap[key] = new Dictionary<int,string>();
+        }
+        timeMap[key][timestamp] = value;
+    }
+    
+    public string Get(string key, int timestamp) {
+        if(!timeMap.ContainsKey(key)) {
+            return "";
+        }
+        
+        for(int currentTime = timestamp; currentTime >= 1; currentTime--) {
+            if(timeMap[key].ContainsKey(currentTime)) {
+                return timeMap[key][currentTime];
+            }
+        }
+        
+        return "";
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.Set(key,value,timestamp);
+ * string param_2 = obj.Get(key,timestamp);
+ */
